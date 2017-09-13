0927de4a5a3402c7f69917cc43d81dae0cd5e432
Francis <ancis.arcede@gmail.com>
2017-09-12 20:02:47 -0400

    oh my lawd

--- build/app2.js
+++ build/app2.js
@@ -1,24 +1,4 @@
-//IMPORT REACT DEPENDENCIES
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import $ from 'jquery';
-
-//IMPORT COMPONENTS
-import Main from './components/Main';
-import Buttons from './components/Buttons';
-import Footer from './components/Footer';
-import Form from './components/Form';
-import List from './components/List';
-import MainMap from './components/MainMap';
-import MiniMap from './components/MiniMap';
-import Navbar from './components/Navbar';
-import Report from './components/Report';
-import Search from './components/Search';
-
-//RENDER THE MAIN APP PAGE
-const App = () =>{
- // We'll be rewriting the table's data frequently, so let's make our code more DRY
+// We'll be rewriting the table's data frequently, so let's make our code more DRY
 // by writing a function that takes in 'animals' (JSON) and creates a table body
 function displayResults(animals) {
   // First, empty the table
@@ -100,7 +80,3 @@ $("#typeofHarassment-sort").on("click", function() {
     displayResults(data);
   });
 });
-}
-
-//DEFAULT APP TO BE USED IN INDEX.JS
-export default App;
