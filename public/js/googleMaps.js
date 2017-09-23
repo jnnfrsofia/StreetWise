337b3c2878f8f7d91b596c76b9bec8b45268738a
Jurabek <spiderman1984@mail.ru>
2017-09-23 00:15:00 -0400

    changed the maps background

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,57 +1,9 @@
-  $("#gmimap0").click(function() {
-      $('form').css('display', 'block');
-      console.log("mapclicked");
-      $('#currentLocMap').css('display', 'none');
-
-  });
-
-  // function displayForm() {
-  //     $('form').css('display', 'block');
-  //       console.log("marker clicked");
-  //       $('#currentLocMap').css('display', 'none');
-  // }
-
   $('#submitForm').click(function() {
       $('#currentLocMap').css('display', 'block');
       $('form').css('display', 'none');
   })
 
-  var map, infoWindow;
-
-  function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 15
-      });
-      infoWindow = new google.maps.InfoWindow;
-
-      // Try HTML5 geolocation.
-      if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-              var pos = {
-                  lat: position.coords.latitude,
-                  lng: position.coords.longitude
-              };
-
-              infoWindow.setPosition(pos);
-              infoWindow.setContent('Location found.');
-              infoWindow.open(map);
-              map.setCenter(pos);
-          }, function() {
-              handleLocationError(true, infoWindow, map.getCenter());
-          });
-      } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-      }
-  }
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(browserHasGeolocation ?
-          'Error: The Geolocation service failed.' :
-          'Error: Your browser doesn\'t support geolocation.');
-      infoWindow.open(map);
-  }
+  
   $(document).ready(function() {
 
       // Google maps that shows where you are at
