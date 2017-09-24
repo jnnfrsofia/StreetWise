5aa1d6560a1e1924d0d6936152cef670779a56b7
khendo888 <khendo888@gmail.com>
2017-09-23 21:11:41 -0400

    Modified logic.js to create map and add multiple markers. Also modified index.html

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,36 +1,38 @@
-// ajax call to /api/incidents inside onclick 
+//Load map
 
 $(document).ready(function() {
-	$("#incidents").on("click", function(e) {
-		e.preventDefault();
-		console.log("Clicked the button");
-		console.log(window.location.origin)
-		
-		var someShitContainer = document.getElementById("someshit");
+    var mapCenter = { lat: 40.7280556, lng: -74.0780556 };
 
-		var queryURL = window.location.origin + "/api/incidents"
-		console.log(queryURL);
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: mapCenter
+    });
 
-		$.ajax({
-			url: queryURL,
-			method: "GET"
-		}).done(function(response){
-			console.log(response);
 
-			for (var i=0; i<response.length; i++){
-				console.log(response[i]);
-				someShitContainer.append(response[i].Description + "\n")
-			}
-		});
-	})
+    var queryURL = window.location.origin + "/api/incidents"
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response) {
+        console.log(response);
+
+        for (var i = 0; i < response.length; i++) {
+            console.log(response[i]);
+            var lat = parseFloat(response[i].Latitude);
+            var lng = parseFloat(response[i].Longitude);
+            console.log(lat);
+            console.log(lng);
+            var myLatLng = { lat, lng };
+            console.log(myLatLng);
+            var details = "Type " + response[i].HarassmentType + " Description " + response[i].Description;
+            var marker = new google.maps.Marker({
+                map: map,
+                position: myLatLng,
+                title: details
+            });
+
+        }
+    });
+   
 });
-
-// ====================================
-// This was here in the origin logic.js
-// i saved it in case someone needs it.
-// ====================================
-
-// $(document).ready(function() {
-//     $('select').material_select();
-
-// });
